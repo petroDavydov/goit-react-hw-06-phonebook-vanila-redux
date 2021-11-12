041814734116f1ce169f5595a20488910e5ba3dd
petroDavydov <petrodavydov@gmail.com>
2021-11-12 02:07:42 +0200

    restart 10

--- src/redux/Phonebook/phone-selectors.js
+++
@@ -1,9 +0,0 @@
-export const getContacts = state => state.contacts.contactItems;
-export const getFilter = state => state.contacts.filter;
-export const getFiltredContacts = state => {
-  const lowerCasedFilter = getFilter(state).toLowerCase();
-  const filteredContacts = getContacts(state).filter(({ name }) =>
-    name.toLowerCase().includes(lowerCasedFilter),
-  );
-  return filteredContacts;
-};
